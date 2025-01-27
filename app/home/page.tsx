cd87c830f2b6146d1100e049bd16e7dd1239cfaf
JRodrigoRC <85160577+RodrigoRoCo@users.noreply.github.com>
2025-01-27 18:28:14 +0100

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -49,15 +49,13 @@ export default function Home() {
 
       <h1 className="text-3xl font-bold mb-8">Gestión de Películas y Salas</h1>
       <div className="flex gap-8">
-        <Link href="/crear-pelicula" className="bg-blue-500 text-white px-4 py-2 rounded">
+        <Link href="/pelicula" className="bg-blue-500 text-white px-4 py-2 rounded">
           Crear Película
         </Link>
-        <Link href="/crear-sala" className="bg-green-500 text-white px-4 py-2 rounded">
+        <Link href="/sala" className="bg-green-500 text-white px-4 py-2 rounded">
           Crear Sala
         </Link>
       </div>
     </main>
   );
 }
-
-
